b75ee53f776e68f0527a2d1b844f0bfe63959cee
Abhish7k <abhishekbhosale.dev@gmail.com>
2024-09-06 19:59:22 +0530

    feat: next auth setup done
    set up the next auth for google and github auth

--- auth.ts
+++ auth.ts
@@ -1,7 +1,11 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
+import { db } from "./lib/db";
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(db),
+
   providers: [GitHub, Google],
 });
