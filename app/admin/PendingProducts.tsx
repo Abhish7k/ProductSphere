bf8ea4e0299228c439933432420dbe15682b2b1a
Abhish7k <abhishekbhosale.dev@gmail.com>
2024-10-22 14:37:30 +0530

    feat: admin dash
    added the view product, activate and reject product modals

--- app/admin/PendingProducts.tsx
+++ app/admin/PendingProducts.tsx
@@ -1,4 +1,10 @@
+"use client";
+
+import ActivateProductModal from "@/components/ui/modal/ActivateProductModal";
+import ProductModal from "@/components/ui/modal/ProductModal";
+import RejectProductModal from "@/components/ui/modal/RejectProductModal";
 import Image from "next/image";
+import { useState } from "react";
 import { PiCheckCircle, PiXCircle } from "react-icons/pi";
 
 interface PendingProductsProps {
@@ -10,6 +16,14 @@ const PendingProducts: React.FC<PendingProductsProps> = ({
   pendingProducts,
   authenticatedUser,
 }) => {
+  const [currentProduct, setCurrentProduct] = useState<any>(null);
+
+  const [viewProductModalVisible, setViewProductModalVisible] = useState(false);
+  const [activateProductModalVisible, setActivateProductModalVisible] =
+    useState(false);
+  const [rejectProductModalVisible, setRejectProductModalVisible] =
+    useState(false);
+
   const formattedProducts = pendingProducts?.map((products: any) => {
     const {
       id,
@@ -55,13 +69,32 @@ const PendingProducts: React.FC<PendingProductsProps> = ({
 
   console.log("Pending products -> ", pendingProducts);
 
+  const handleViewProductModal = (product: any) => {
+    const formattedProduct = formattedProducts.find(
+      (formattedProduct: any) => formattedProduct.id === product.id
+    );
+
+    setCurrentProduct(formattedProduct);
+    setViewProductModalVisible(true);
+  };
+
+  const handleActivateProductModal = (product: any) => {
+    setCurrentProduct(product);
+    setActivateProductModalVisible(true);
+  };
+
+  const handleRejectProductModal = (product: any) => {
+    setCurrentProduct(product);
+    setRejectProductModalVisible(true);
+  };
+
   return (
     <div>
       <div className="flex flex-col w-full my-5">
         {formattedProducts?.map((product: any) => (
           <div
             key={product.id}
-            className="flex border rounded-md p-4 justify-between items-center"
+            className="flex items-center rounded-md p-4 justify-between border"
           >
             <div className="flex gap-x-6 items-center">
               <Image
@@ -69,48 +102,70 @@ const PendingProducts: React.FC<PendingProductsProps> = ({
                 alt="logo"
                 width={200}
                 height={200}
-                className="w-10 md:w-20 rounded-md cursor-pointer"
+                className="w-14 h-14 md:w-20 md:h-20 rounded-md cursor-pointer"
               />
 
-              <div className="space-y-2">
-                <h1 className="text-2xl font-bold">{product.name} </h1>
-                <p className="hidden md:flex text-gray-500 text-sm pr-6">
+              <div className="space-y-1">
+                <h1 className="text md:text-2xl font-bold capitalize">
+                  {product.name}{" "}
+                </h1>
+                <p className="hidden md:flex text-gray-500 text-xs lg:text-sm pr-6">
                   {product.description}
                 </p>
-                <div className="hidden md:flex text-gray-500 font-semibold">
+                <div className="hidden md:flex text-gray-500 font-medium">
                   Release Date : {product.releaseDate}
                 </div>
               </div>
             </div>
 
-            <div className="flex items-center gap-2 md:gap-x-4 justify-center">
+            <div className="flex flex-col lg:flex-row items-center gap-2 md:gap-x-4 justify-center">
               <button
-                // onClick={() => handleViewProductModal(product)}
-                className="bg-[#ff6154]
-                 text-white px-4 py-2 text-center
-                  text-sm  rounded-md "
+                onClick={() => handleViewProductModal(product)}
+                className="bg-[#ff6154] text-white px-4 py-2 text-center text-sm rounded-md hover:bg-[#ff4437] transition-all duration-300"
               >
-                View Product
+                <span className="hidden sm:block">View Product</span>
+                <span className="block sm:hidden">X</span>
               </button>
 
-              <button
-                // onClick={() => handleActivateProductModal(product)}
-                className="bg-emerald-100 text-white
-                         px-4 py-2 text-center text-sm rounded-md"
-              >
-                <PiCheckCircle className="text-xl text-emerald-500" />
-              </button>
+              <div className="flex items-center gap-2">
+                <button
+                  onClick={() => handleActivateProductModal(product)}
+                  className="bg-emerald-100 text-white px-4 py-2 text-center text-sm rounded-md hover:bg-emerald-200 transition-all duration-300"
+                >
+                  <PiCheckCircle className="text-xl text-emerald-500" />
+                </button>
 
-              <button
-                // onClick={() => handleRejectProductModal(product)}
-                className="bg-red-100 text-white
-                px-4 py-2 text-center text-sm rounded-md"
-              >
-                <PiXCircle className="text-xl text-red-500" />
-              </button>
+                <button
+                  onClick={() => handleRejectProductModal(product)}
+                  className="bg-red-100 text-white px-4 py-2 text-center text-sm rounded-md hover:bg-red-200 transition-all duration-300"
+                >
+                  <PiXCircle className="text-xl text-red-500" />
+                </button>
+              </div>
             </div>
           </div>
         ))}
+
+        <ProductModal
+          visible={viewProductModalVisible}
+          setVisible={setViewProductModalVisible}
+        >
+          product modal
+        </ProductModal>
+
+        <ActivateProductModal
+          visible={activateProductModalVisible}
+          setVisible={setActivateProductModalVisible}
+        >
+          ActivateProductModal
+        </ActivateProductModal>
+
+        <RejectProductModal
+          visible={rejectProductModalVisible}
+          setVisible={setRejectProductModalVisible}
+        >
+          RejectProductModal
+        </RejectProductModal>
       </div>
     </div>
   );
