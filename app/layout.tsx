3b19e556c348d93fd8f042419a02bd5648de652c
Abhish7k <abhishekbhosale.dev@gmail.com>
2024-11-09 15:10:44 +0530

    chore: favicon
    changed the favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+      </head>
+
       <body className={font.className}>
         <NextTopLoader showSpinner={false} />
         {children}
