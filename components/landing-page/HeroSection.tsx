64377be5b4b96440b80b35bbf04f6d1b4ad7927b
Abhish7k <abhishekbhosale.dev@gmail.com>
2024-11-09 14:52:00 +0530

    feat: hero section
    added an animated btn from magicui for enhaced ui

--- components/landing-page/HeroSection.tsx
+++ components/landing-page/HeroSection.tsx
@@ -1,6 +1,9 @@
+import { ChevronRight } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { GoArrowRight } from "react-icons/go";
+import AnimatedGradientText from "../ui/animated-gradient-text";
+import { cn } from "@/lib/utils";
 
 const HeroSection = () => {
   return (
@@ -12,9 +15,28 @@ const HeroSection = () => {
       transition-all
       "
     >
-      <div className="pt-24 md:pt-28 flex flex-col items-center w-full text-center">
-        <h1 className="text-3xl sm:text-5xl lg:text-6xl md:max-w-2xl py-2 md:py-0 lg:!leading-[4rem] font-medium tracking-tight transition-all">
-          Your ultimate platform to discover new products
+      <div className="pt-24  flex flex-col items-center w-full text-center">
+        {/*  */}
+        <div className="z-10 mb-5 md:mb-10 flex items-center justify-center">
+          <AnimatedGradientText>
+            🎉
+            <span
+              className={cn(
+                `ml-2 inline animate-gradient 
+                bg-gradient-to-r from-[#ff8c00] via-[#6b23b4] to-[#ff8c00] bg-[length:var(--bg-size)_100%] bg-clip-text text-transparent
+                text-xs md:text-sm font-medium
+                `
+              )}
+            >
+              Introducing ProductSphere
+            </span>
+            <ChevronRight className="ml-1 size-3 transition-all duration-300 ease-in-out group-hover:translate-x-0.5 will-change-transform" />
+          </AnimatedGradientText>
+        </div>
+
+        {/*  */}
+        <h1 className="text-3xl sm:text-5xl lg:text-6xl md:max-w-2xl py-2 md:py-0 lg:!leading-[3.75rem] font-medium tracking-tight transition-all">
+          Your ultimate platform to <br /> discover new products
         </h1>
 
         <p className="mt-2 md:mt-4 max-w-sm md:max-w-[26rem] md:text-lg text-foreground/70 font-medium leading-6 transition-all">
@@ -24,7 +46,7 @@ const HeroSection = () => {
 
         <Link
           href="/products"
-          className="group relative mt-10 px-6 py-1.5 flex transform items-center justify-center gap-2 overflow-hidden whitespace-nowrap rounded-md border border-white text-white bg-indigo-500 font-medium transition-all 
+          className="group relative mt-10 px-6 py-1.5 flex transform items-center justify-center gap-2 overflow-hidden whitespace-nowrap rounded-md text-white bg-indigo-500 font-medium transition-all 
           duration-300 
           hover:ring-2 hover:ring-offset-1 hover:ring-indigo-500
           "
@@ -42,7 +64,9 @@ const HeroSection = () => {
       </div>
 
       <div className="relative mt-5">
-        <div className="absolute top-20 md:top-[12%] left-1/2 gradient w-3/4 -translate-x-1/2 h-1/4 md:h-1/3 inset-0 blur-[5rem]"></div>
+        {/* <div className="absolute top-20 md:top-[12%] left-1/2 gradient w-3/4 -translate-x-1/2 h-1/4 md:h-1/3 inset-0 blur-[5rem]"></div> */}
+
+        <div className="absolute top-1/2 left-1/2 -z-10 gradient w-full -translate-x-1/2 h-3/4 -translate-y-1/2 inset-0 blur-[10rem]"></div>
 
         <div
           className="relative mx-auto max-w-5xl rounded-xl lg:rounded-[32px] border border-neutral-200/50 p-2 backdrop-blur-lg border-neutral-300 bg-neutral-200/20 
