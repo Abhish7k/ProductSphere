81741dcc8a6a9b3cad49e2f9601ad6b8f2c3d945
Abhish7k <abhishekbhosale.dev@gmail.com>
2024-11-10 20:55:05 +0530

    feat: landing page: how it works section

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,12 @@
 import HeroSection from "@/components/landing-page/HeroSection";
+import { HowItWorksSection } from "@/components/landing-page/HowItWorksSection";
 
 export default async function Home() {
   return (
     <main className="">
       <HeroSection />
 
-      <div className="mt-40"></div>
+      <HowItWorksSection />
     </main>
   );
 }
