744f3a58482f700ea7340b4f914c230573f7d7c6
Abhish7k <abhishekbhosale.dev@gmail.com>
2024-11-10 13:36:07 +0530

    style: navbar
    navbar sign in button styles enhanced

--- components/navbar/AuthButtons.tsx
+++ components/navbar/AuthButtons.tsx
@@ -1,25 +1,12 @@
 import React from "react";
-import { Button } from "../ui/button";
 
 const AuthButtons = () => {
   return (
-    <div className="flex items-center gap-2 transition-all">
-      <Button
-        className="px-3 sm:px-4 text-sm sm:text-base 
-      text-white bg-indigo-500 hover:bg-indigo-600 
-        transition-all duration-300"
-      >
-        Sign Up
-      </Button>
+    <button className="group relative px-2 py-1 sm:px-4 sm:py-1.5 flex transform items-center justify-center gap-2 overflow-hidden whitespace-nowrap rounded-md border border-white text-white bg-indigo-500 font-medium transition-all duration-300 active:scale-90">
+      <span className="relative z-10 flex items-center gap-2">Sign In</span>
 
-      <Button
-        variant="outline"
-        className="px-3 sm:px-4 text-sm sm:text-base 
-        transition-all duration-300"
-      >
-        Sign In
-      </Button>
-    </div>
+      <div className="ease-&lsqb;cubic-bezier(0.19,1,0.22,1)&rsqb; absolute -left-[75px] -top-[50px] -z-10 h-[155px] w-8 rotate-[35deg] bg-white opacity-20 transition-all duration-500 group-hover:left-[120%]" />
+    </button>
   );
 };
 
